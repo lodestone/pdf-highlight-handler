1eb3e459dcef05a85e434cf881dfee155a287447
Matt Petty <matt@kizmeta.com>
2011-12-21 19:34:56 -0600

    Remove comment

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -12,10 +12,6 @@ class Highlight
 
   def self.from_hash(hash)
     highlight = Highlight.new(hash)
-    # highlight.text = hash['text']
-    # highlight.page = hash['page']
-    # highlight.created_at = hash['created_at']
-    # highlight
   end
 
   def self.from_hashes(hashes)
