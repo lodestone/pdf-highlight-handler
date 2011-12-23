e2bf55aae98ddb511cb76de45f9b3537a96e820b
Matt Petty <matt@kizmeta.com>
2011-12-23 07:34:17 -0600

    Set more application config variables to enable heroku asset serving

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,8 @@ module Annotation
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
