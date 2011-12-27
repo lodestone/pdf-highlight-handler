be0e1a6db5676e33b2b8dec6ecccf7ae992507e1
Matt Petty <matt@kizmeta.com>
2011-12-27 07:05:23 -0600

    Remove unused method

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -19,22 +19,4 @@ class Highlight
     b.score <=> self.score
   end
 
-  # TODO Look at publication
-  def self.from_hash(hash, options={})
-    hls = Highlight.find_all_by_publication_id_and_user_id(options[:publication].id, options[:user].id)
-    highlight = hls.detect {|hl| hl.text == hash['text']}
-    highlight = Highlight.new if highlight.blank?
-    highlight.user = options[:user]
-    highlight.text = hash['text']
-    highlight.page = hash['page']
-    highlight.created_at = hash['created_at']
-    hash['rects'].each do |h|
-      rect = h[1].first # NASTY XML HOBBITSES
-      fragment = Fragment.create_from_rect(rect)
-      highlight.fragments << fragment
-    end
-    highlight.save
-    highlight
-  end
-
 end
